c6adbd4fc08d93dbd1517d095c56b39d2cd55737
vivekvardhan2324 <64770728+vivekvardhan2324@users.noreply.github.com>
2026-04-19 10:53:21 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "DevOps-Instance"
+  }
+}
